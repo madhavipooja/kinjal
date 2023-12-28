eebe6218c7a0201ed45cb3d3d8a293eb37e5b6b8
pooja <152240500+pooja-71414@users.noreply.github.com>
2023-12-28 16:03:33 +0530

    Update string.js

--- basics/string.js
+++ basics/string.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
-const repoCount = 50
+const name = "pooja"
+const repoCount = 12
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('pooja-ph-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +22,11 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   pooja    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://hitesh.com/pooja%20parmar"
 
 console.log(url.replace('%20', '-'))
 
